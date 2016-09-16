f4293764788d4056717e420bb1b3abf478515013
Corrandoo <ivan260302@yandex.ru>
2016-09-16 19:31:39 +0500

    Added mySQL

--- src/main/java/xyz/ikuznetsov/qubobot/Main.java
+++ src/main/java/xyz/ikuznetsov/qubobot/Main.java
@@ -6,6 +6,8 @@ import org.telegram.telegrambots.TelegramBotsApi;
 public class Main {
     public static void main(String[] args) {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        HibernateSessionFactory.getSessionFactory().openSession();
+        TelegramBotsApi superbot = new TelegramBotsApi();
         try {
             telegramBotsApi.registerBot(new TelegramBot());
         } catch (TelegramApiException e) {
