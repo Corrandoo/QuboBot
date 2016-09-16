89d4ac42146f936915156e997416837c4ad1a0c8
Annenkoff <contact@annenkov.me>
2016-09-16 17:09:02 +0300

    Created main() method.

--- src/main/java/xyz/ikuznetsov/qubobot/Main.java
+++ src/main/java/xyz/ikuznetsov/qubobot/Main.java
@@ -1,4 +1,15 @@
 package xyz.ikuznetsov.qubobot;
 
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+
 public class Main {
+    public static void main(String[] args) {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new TelegramBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
