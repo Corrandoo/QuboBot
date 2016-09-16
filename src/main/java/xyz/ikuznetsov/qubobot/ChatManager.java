8a33dabfcc58b84f32ad48d47459375a71b4ef7d
Corrandoo <ivan260302@yandex.ru>
2016-09-16 20:50:52 +0500

    Started work on database

--- src/main/java/xyz/ikuznetsov/qubobot/ChatManager.java
+++ src/main/java/xyz/ikuznetsov/qubobot/ChatManager.java
@@ -1,6 +1,7 @@
 package xyz.ikuznetsov.qubobot;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.telegram.telegrambots.api.objects.Message;
 import xyz.ikuznetsov.qubobot.dao.UsersEntity;
 
@@ -12,5 +13,11 @@ public class ChatManager {
         usersEntity.setId(message.getChatId());
         session.save(usersEntity);
         session.getTransaction().commit();
+    }
+    public static void checkChat(Message message){
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+        long id = message.getChatId();
+        Query query = session.createQuery("FROM UsersEntity WHERE id=:id");
+        query.setParameter("id", id);
     }
 }
