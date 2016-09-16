a59e17d08117fc24e4654779755edf5042ddef19
Annenkoff <contact@annenkov.me>
2016-09-16 17:33:07 +0300

    Edited HibernateSessionFactory.

--- src/main/java/xyz/ikuznetsov/qubobot/Configuration.java
+++ src/main/java/xyz/ikuznetsov/qubobot/Configuration.java
@@ -1,5 +1,8 @@
 package xyz.ikuznetsov.qubobot;
 
+/**
+ * Класс-конфигурация с вынесенными полями для настройки.
+ */
 public class Configuration {
     public static final String BOT_USERNAME = "QuboBot";
     public static final String BOT_TOKEN = "295320548:AAFZFjB2U7FAQBDqRRV24PtNM67uWDTpSxg";
--- src/main/java/xyz/ikuznetsov/qubobot/HibernateSessionFactory.java
+++ src/main/java/xyz/ikuznetsov/qubobot/HibernateSessionFactory.java
@@ -1,25 +1,24 @@
 package xyz.ikuznetsov.qubobot;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateSessionFactory {
-    private static ServiceRegistry serviceRegistry;
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory sessionFactory = buildSessionFactory();
 
-    private static SessionFactory buildSessionFactory() {
+    protected static SessionFactory buildSessionFactory() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
         try {
-            Configuration configuration = new Configuration();
-            configuration.configure();
-            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-
-            return configuration.buildSessionFactory(serviceRegistry);
-        } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new ExceptionInInitializerError("Initial SessionFactory failed" + e);
         }
+        return sessionFactory;
     }
 
     public static SessionFactory getSessionFactory() {
