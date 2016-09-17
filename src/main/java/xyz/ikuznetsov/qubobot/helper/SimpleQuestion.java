c1ff6f1bebeee9ad636b8017011d0e6cb6feabd5
Corrandoo <ivan260302@yandex.ru>
2016-09-17 20:06:18 +0500

    Added and edited class xyz.ikuznetsov.qubobot.helper.SimpleQuestion

--- src/main/java/xyz/ikuznetsov/qubobot/helper/SimpleQuestion.java
+++ src/main/java/xyz/ikuznetsov/qubobot/helper/SimpleQuestion.java
@@ -3,15 +3,17 @@ package xyz.ikuznetsov.qubobot.helper;
 import org.telegram.telegrambots.api.objects.Message;
 
 public class SimpleQuestion {
+    // Класс, определяющий признаки простого вопроса в сообщении для передачи его парсеру
+
     private int score = 0;
     private boolean isQuestion;
-    public boolean simpleQuestionDetector(Message message){
+    public boolean simpleQuestionDetector(Message message){ // Основной метод. Единственный публичный в классе.
         String text = message.getText().toLowerCase();
         detector(text);
         decider();
         return isQuestion;
     }
-    private void detector(String text){
+    private void detector(String text){ // Метод с основной логикой
         if(text.contains("?"))
             score += 3;
         if(text.contains("что") || text.contains("кто")) {
@@ -25,7 +27,7 @@ public class SimpleQuestion {
             score += 1;
 
     }
-    private void decider(){
+    private void decider(){ // Метод, решающий, вопрос это или нет по количеству баллов.
         if(score >= 4)
             isQuestion = true;
         else
